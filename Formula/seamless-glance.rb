b07b57332b8f6f9846bb229ce715480ed9fb554a
Brandon Corbett <Bccorb@users.noreply.github.com>
2026-01-02 18:09:51 -0500

    fix: corrected repo

--- Formula/seamless-glance.rb
+++ Formula/seamless-glance.rb
@@ -4,10 +4,10 @@ class SeamlessGlance < Formula
   version "0.1.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/fellscode/seamless-glance-distro/releases/download/v0.1.0-beta.8/seamless-glance-0.1.0-beta.8-aarch64-apple-darwin"
+    url "https://github.com/fells-code/seamless-glance-distro/releases/download/v0.1.0-beta.8/seamless-glance-0.1.0-beta.8-aarch64-apple-darwin"
     sha256 "95ca51e7e955a0268939713bee6706ebdfafbf96cb91dc16f9f20639596aaf7e"
   elsif OS.mac?
-    url "https://github.com/fellscode/seamless-glance-distro/releases/download/v0.1.0-beta.8/seamless-glance-0.1.0-beta.8-x86_64-apple-darwin"
+    url "https://github.com/fells-code/seamless-glance-distro/releases/download/v0.1.0-beta.8/seamless-glance-0.1.0-beta.8-x86_64-apple-darwin"
     sha256 "REPLACE_X86_SHA256"
   else
     odie "Seamless Glance is currently supported on macOS via Homebrew"
