1e3cbf06a99fefa422865f4e610b3d3b066e52c7
Brandon Corbett <Bccorb@users.noreply.github.com>
2026-01-07 23:46:18 -0500

    chore: version bump to v0.1.0-beta.14

--- Formula/seamless-glance.rb
+++ Formula/seamless-glance.rb
@@ -2,20 +2,21 @@ class SeamlessGlance < Formula
   desc "Fast, read-only AWS TUI for cloud infrastructure visibility"
   homepage "https://seamlessglance.com"
   license "Proprietary"
-  version "0.1.0-beta.13"
+  version "0.1.0-beta.14"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/fells-code/seamless-glance-distro/releases/download/v0.1.0-beta.13/seamless-glance-0.1.0-beta.13-aarch64-apple-darwin"
-    sha256 "51d96d4209b9881b7b9b336f971e8219af8329762ebc71092d30cdb3c0eb0ff4"
+    url "https://github.com/fells-code/seamless-glance-distro/releases/download/v0.1.0-beta.14/seamless-glance-0.1.0-beta.14-aarch64-apple-darwin"
+    sha256 "36be7c618d15f9b5e485acab334f07e11df8dc2066163e6ee29e4a3958d6437d"
   elsif OS.mac?
-    url "https://github.com/fells-code/seamless-glance-distro/releases/download/v0.1.0-beta.13/seamless-glance-0.1.0-beta.13-x86_64-apple-darwin"
-    sha256 "dedef376874438507a321a18ea6e9600214b698619eb6616d7e0b000dabb033d"
+    url "https://github.com/fells-code/seamless-glance-distro/releases/download/v0.1.0-beta.14/seamless-glance-0.1.0-beta.14-x86_64-apple-darwin"
+    sha256 "e0e82bbe125d386f52c60eccf8e1a3a8dca2f4551df483f9e7763e74eae5dac8"
   else
     odie "Seamless Glance is currently supported on macOS via Homebrew"
   end
 
-  def install
-     bin.install Dir["seamless-glance-*"].first => "seamless-glance"
+ def install
+    bin.install "seamless-glance"
+    bin.install_symlink "seamless-glance" => "glance"
   end
 
   test do
